d83743ba429aaba91047a7197c3787f5c92cf11f
Etienne Hurand <informatique@etiennehurand.fr>
2022-03-02 10:47:26 +0100

    Create ViewModels for each content

---
+++ BookshopApp/ViewModels/CdeClientContentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookshopApp.ViewModels
+{
+    public class CdeClientContentViewModel
+    {
+        public override string ToString()
+        {
+            return "CdeClient";
+        }
+    }
+}
---
+++ BookshopApp/ViewModels/HistoContentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookshopApp.ViewModels
+{
+    public class HistoContentViewModel
+    {
+        public override string ToString()
+        {
+            return "Histo";
+        }
+    }
+}
