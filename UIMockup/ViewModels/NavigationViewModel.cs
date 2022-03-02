1f8e3f85a4bb3b567cdf9079768d315efeab4fcb
Etienne Hurand <informatique@etiennehurand.fr>
2022-03-02 09:48:13 +0100

    Finalize the navigation in UIMockup: Each button displays the corresponding content

--- UIMockup/ViewModels/NavigationViewModel.cs
+++ UIMockup/ViewModels/NavigationViewModel.cs
@@ -13,8 +13,11 @@ namespace UIMockup.ViewModels
     {
 
         public ICommand CaisseCommand { get; set; }
-
+        public ICommand AchatCommand { get; set; }
+        public ICommand CdeClientCommand { get; set; }
         public ICommand StockCommand { get; set; }
+        public ICommand HistoCommand { get; set; }
+        public ICommand AdminCommand { get; set; }
 
         private object selectedContentViewModel = new StockContentViewModel();
         private object selectedMenuViewModel = new StockMenuViewModel();
@@ -58,12 +61,15 @@ namespace UIMockup.ViewModels
         public NavigationViewModel()
 
         {
-
             CaisseCommand = new BaseCommand(OpenCaisse);
-
+            AchatCommand = new BaseCommand(OpenAchat);
+            CdeClientCommand = new BaseCommand(OpenCdeClient);
             StockCommand = new BaseCommand(OpenStock);
+            HistoCommand = new BaseCommand(OpenHisto);
+            AdminCommand = new BaseCommand(OpenAdmin);
 
         }
+        #region Open methods
 
         private void OpenCaisse(object obj)
 
@@ -71,6 +77,24 @@ namespace UIMockup.ViewModels
 
             SelectedContentViewModel = new CaisseContentViewModel();
             SelectedMenuViewModel = new CaisseMenuViewModel();
+
+        }
+
+        private void OpenAchat(object obj)
+
+        {
+
+            SelectedContentViewModel = new AchatContentViewModel();
+            SelectedMenuViewModel = new AchatMenuViewModel();
+
+        }
+
+        private void OpenCdeClient(object obj)
+
+        {
+
+            SelectedContentViewModel = new CdeClientContentViewModel();
+            SelectedMenuViewModel = new CdeClientMenuViewModel();
 
         }
 
@@ -82,6 +106,25 @@ namespace UIMockup.ViewModels
             SelectedMenuViewModel = new StockMenuViewModel();
 
         }
+
+        private void OpenHisto(object obj)
+
+        {
+
+            SelectedContentViewModel = new HistoContentViewModel();
+            SelectedMenuViewModel = new HistoMenuViewModel();
+
+        }
+
+        private void OpenAdmin(object obj)
+
+        {
+
+            SelectedContentViewModel = new AdminContentViewModel();
+            SelectedMenuViewModel = new AdminMenuViewModel();
+
+        }
+        #endregion
 
         public event PropertyChangedEventHandler PropertyChanged;
 
