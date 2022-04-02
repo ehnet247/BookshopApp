b9902e0bf222ef18df8d415fd3fd86ac809af2f9
Etienne Hurand <informatique@etiennehurand.fr>
2022-04-02 16:46:27 +0200

    Begin to implement the Achat ViewModel

--- BookshopApp/ViewModels/ViewModelAchat.cs
+++ BookshopApp/ViewModels/ViewModelAchat.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 using CommunityToolkit.Mvvm.ComponentModel;
+using BookshopApp.Model;
+using System.Windows.Input;
 
 namespace BookshopApp.ViewModels
 {
-    public class ViewModelAchat : ObservableRecipient
+    internal class ViewModelAchat : ObservableRecipient
     {
+        private LivreDbContext DbContext;
+        public ICommand CommandEvaluer { get; set; }
+        private LivreString livreStringAAcheter;
+        public LivreString LivreStringAAcheter
+        {
+            get
+            {
+                return livreStringAAcheter;
+            }
+            set
+            {
+                livreStringAAcheter = value;
+                OnPropertyChanged("LivreStringAAcheter");
+            }
+        }
         public override string ToString()
         {
             return "Achat";
+        }
+
+        public ViewModelAchat()
+        {
+            CommandEvaluer = new BaseCommand(EvaluerLivre);
+            DbContext = new LivreDbContext();
+        }
+
+        private void EvaluerLivre(object obj)
+        {
+            if (LivreStringAAcheter.Isbn13 != string.Empty)
+            {
+            }
         }
     }
 }
